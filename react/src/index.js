4d659aa8852d37d30333b66fafe5a4bf308b231c
杨海涛 <yannis_123@live.com>
2019-10-07 18:42:04 +0800

    modify

--- react/src/index.js
+++ react/src/index.js
@@ -1,58 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import * as storage from '@/library/utils/storage';
+import * as sxRedux from '@/library/redux';
+import App from './App';
+import handleSuccess from './commons/handle-success';
+import handleError from './commons/handle-error';
+import {configureStore} from './models';
 import * as serviceWorker from './serviceWorker';
-import Page from './Page';
-import * as apis from './axios';
-// import { AppContainer } from 'react-hot-loader';
-import { AlitaProvider, setConfig } from 'redux-alita';
-import './style/lib/animate.css';
-import './style/antd/index.less';
-import './style/index.less';
+import {getLoginUser} from "./commons";
+import './index.css';
 
-setConfig(apis);
-// const render = Component => { // 增加react-hot-loader保持状态刷新操作，如果不需要可去掉并把下面注释的打开
-//     ReactDOM.render(
-//         <AppContainer>
-//             <Provider store={store}>
-//                 <Component store={store} />
-//             </Provider>
-//         </AppContainer>
-//         ,
-//         document.getElementById('root')
-//     );
-// };
+// dev 模式开启mock
+if (process.env.NODE_ENV === 'development') {
+    require('./mock/index');
+    console.log('current mode is development, mock is enabled');
+}
 
-// render(Page);
+const currentUser = getLoginUser() || {};
 
-// Webpack Hot Module Replacement API
-// if (module.hot) {
-//     // 隐藏You cannot change <Router routes>; it will be ignored 错误提示
-//     // react-hot-loader 使用在react-router 3.x上引起的提示，react-router 4.x不存在
-//     // 详情可参照https://github.com/gaearon/react-hot-loader/issues/298
-//     const orgError = console.error; // eslint-disable-line no-console
-//     console.error = (...args) => { // eslint-disable-line no-console
-//         if (args && args.length === 1 && typeof args[0] === 'string' && args[0].indexOf('You cannot change <Router routes>;') > -1) {
-//             // React route changed
-//         } else {
-//             // Log the error as normally
-//             orgError.apply(console, args);
-//         }
-//     };
-//     module.hot.accept('./Page', () => {
-//         render(Page);
-//     })
-// }
+// 存储初始化 区分不同用户存储的数据
+storage.init({
+    keyPrefix: currentUser.id,
+});
 
-ReactDOM.render(
-    // <AppContainer>
-    <AlitaProvider>
-        <Page />
-    </AlitaProvider>,
-    // </AppContainer>
-    document.getElementById('root')
-);
+sxRedux.init({storage, handleError, handleSuccess});
+
+// models store
+const store = configureStore();
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-serviceWorker.register();
+serviceWorker.unregister();
